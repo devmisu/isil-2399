20e8bc1c078df2e7e37290f25d3d206d62f0e159
leghead <ernestogaspard@gmail.com>
2021-10-12 16:58:55 -0500

    Commit: Task detail layout pending delete button

---
+++ solerajobs/app/src/main/java/pe/solera/solerajobs/ui/main/task/detail/TaskDetailActivity.kt
@@ -0,0 +1,12 @@
+package pe.solera.solerajobs.ui.main.task.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pe.solera.solerajobs.R
+
+class TaskDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_detail)
+    }
+}
