b3494ea5e881bdc4f2af0c574531fcc7b9723998
0x0010b <felix.chacaltana.felix@gmail.com>
2021-11-03 18:32:34 -0500

    Add sequelize config file

--- config/config.js
+++ config/config.js
@@ -1,25 +1,28 @@
 require('dotenv').config()
 
 module.exports = {
-  "development": {
-      "username": process.env.DB_DEV_USERNAME,
-      "password": process.env.DB_DEV_PASSWORD,
-      "database": process.env.DB_DEV_DATABASE,
-      "host": process.env.DB_DEV_HOST,
-      "dialect": "mysql"
+  development: {
+    username: process.env.DEV_DB_USERNAME,
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.DEV_DB_NAME,
+    host: '127.0.0.1',
+    port: 3306,
+    dialect: 'mysql'
   },
-  "test": {
-    "username": process.env.DB_DEV_USERNAME,
-    "password": process.env.DB_DEV_PASSWORD,
-    "database": process.env.DB_DEV_DATABASE,
-    "host": process.env.DB_DEV_HOST,
-    "dialect": "mysql"
-},
-  "production": {
-    "username": process.env.DB_PROD_USERNAME,
-    "password": process.env.DB_PROD_PASSWORD,
-    "database": process.env.DB_PROD_DATABASE,
-    "host": process.env.DB_PROD_HOST,
-    "dialect": "mysql"
+  test: {
+    username: process.env.CI_DB_USERNAME,
+    password: process.env.CI_DB_PASSWORD,
+    database: process.env.CI_DB_NAME,
+    host: '127.0.0.1',
+    port: 3306,
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    port: process.env.PROD_DB_PORT,
+    dialect: 'mysql'
   }
 }
