709f97c80f44fdacab4e6fc3d6deb1905da57ff5
0x0010b <felix.chacaltana.felix@gmail.com>
2021-11-03 19:39:02 -0500

    Add db models

---
+++ models/member.js
@@ -0,0 +1,61 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('member', {
+    'id': {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      primaryKey: true,
+      comment: "null",
+      autoIncrement: true
+    },
+    'firstName': {
+      type: DataTypes.STRING(45),
+      allowNull: false,
+      comment: "null"
+    },
+    'lastName': {
+      type: DataTypes.STRING(45),
+      allowNull: false,
+      comment: "null"
+    },
+    'email': {
+      type: DataTypes.STRING(45),
+      allowNull: false,
+      comment: "null"
+    },
+    'phone': {
+      type: DataTypes.STRING(45),
+      allowNull: false,
+      comment: "null"
+    },
+    'jobId': {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      comment: "null",
+      references: {
+        model: 'job',
+        key: 'id'
+      }
+    },
+    'createdAt': {
+      type: DataTypes.DATE,
+      allowNull: false,
+      comment: "null"
+    },
+    'updatedAt': {
+      type: DataTypes.DATE,
+      allowNull: true,
+      comment: "null"
+    },
+    'deletedAt': {
+      type: DataTypes.DATE,
+      allowNull: true,
+      comment: "null"
+    }
+  }, {
+    tableName: 'member',
+    paranoid: true
+  });
+};
