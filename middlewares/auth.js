8bf11f2bc682f0c24a069399c61f8decdf7ef6e4
0x0010b <felix.chacaltana.felix@gmail.com>
2021-11-05 02:45:33 -0500

    Update authentication middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,23 @@
 const jwt = require('../common/jwt')
 const validator = require('../common/validator')
 
-module.exports = {
+module.exports = (req, res, next) => {
 
-    verifyToken (req, res, next) {
+    try {
 
-        try {
+        const { result, devMessage } = validator.valid(req.headers, ['authorization'])
 
-            const { result, devMessage } = validator.valid(req.headers, ['authorization'])
+        if (!result) throw devMessage
+    
+        const authorization = req.headers['authorization'].split(' ')
+        const token = authorization[1]
+    
+        req.user = jwt.verify(token)
+    
+        return next()   
 
-            if (!result) throw devMessage
-        
-            const authorization = req.headers['authorization'].split(' ')
-            const token = authorization[1]
-        
-            req.user = jwt.verify(token)
-        
-            return next()   
+    } catch(error) {
 
-        } catch(error) {
-
-            return res.status(401).json({ message: error })
-        }
+        return res.status(401).json({ message: error })
     }
 }
