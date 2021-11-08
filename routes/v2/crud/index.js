74b302da3cfd6c4f639145eb78550818d43d916c
0x0010b <felix.chacaltana.felix@gmail.com>
2021-11-08 00:23:02 -0500

    Add Member CRUD

--- routes/v2/crud/index.js
+++ routes/v2/crud/index.js
@@ -8,5 +8,6 @@ routes.get('/', (req, res) => {
 routes.use('/client', require('./client'))
 routes.use('/area', require('./area'))
 routes.use('/job', require('./job'))
+routes.use('/member', require('./member'))
 
 module.exports = routes
