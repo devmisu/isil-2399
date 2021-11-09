b9d49056690cb5eb1a84b6b7f444d2b29c8acc76
0x0010b <felix.chacaltana.felix@gmail.com>
2021-11-09 00:41:34 -0500

    Add save favorite projects service

--- routes/v2/app/favorites.js
+++ routes/v2/app/favorites.js
@@ -5,6 +5,7 @@ const auth = require('../../../middlewares/auth')
 const Client = require('../../../models').client
 const Project = require('../../../models').project
 const Favorite = require('../../../models').favorite_project
+const FavoriteLog = require('../../../models').favorite_project_log
 
 routes.get('/', auth, async (req, res) => {
 
@@ -31,6 +32,50 @@ routes.get('/', auth, async (req, res) => {
 
     } catch(error) {
 
+        res.status(400).json({ message: error })
+    }
+})
+
+routes.post('/', auth, async (req, res) => {
+
+    try {
+
+        if (!Array.isArray(req.body)) throw 'Debes enviar una lista valida.'
+
+        const favorites = await Favorite.findAll({ where: { memberId: req.user.id } })
+
+        await favorites.forEach(async (favorite) => {
+
+            const deletedFavorite = await favorite.destroy()
+
+            await FavoriteLog.create({
+                favoriteProjectId: deletedFavorite.id,
+                memberId: deletedFavorite.memberId,
+                projectId: deletedFavorite.projectId,
+                createdAt: deletedFavorite.createdAt,
+                updatedAt: deletedFavorite.updatedAt,
+                deletedAt: deletedFavorite.deletedAt
+            })
+        })
+
+        await req.body.forEach(async (projectId) => {
+
+            const newFavorite = await Favorite.create({ memberId: req.user.id, projectId: projectId })
+            
+            await FavoriteLog.create({
+                favoriteProjectId: newFavorite.id,
+                memberId: newFavorite.memberId,
+                projectId: newFavorite.projectId,
+                createdAt: newFavorite.createdAt,
+                updatedAt: newFavorite.updatedAt,
+                deletedAt: newFavorite.deletedAt
+            })
+        })
+
+        res.json()
+
+    } catch(error) {
+        
         res.status(400).json({ message: error })
     }
 })
